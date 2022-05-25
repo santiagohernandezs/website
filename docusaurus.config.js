a664a1285eb500d935c202c9a93040a9287eaef2
Santiago Hernandez <santiagooheernandez@gmail.com>
2022-05-24 20:00:27 -0400

    changing title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Biblioteca',
+  tagline: 'Toda la documentación que necesitas',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
